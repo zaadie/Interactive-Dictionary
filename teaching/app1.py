db513d62502e86ed1e2084933a83b5cfa64ae292
zaadie <saadiyafourie@gmail.com>
2019-04-30 17:11:43 +1000

    Defenition function now has suggestions

--- teaching/app1.py
+++ teaching/app1.py
@@ -1,15 +1,30 @@
 import json
+from difflib import get_close_matches
 
-
-data = json.open("data.json")
+data = json.load(open("data.json"))
 
 def translate(w):
+    w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w,data.keys())) > 0:
+        yn = input("Did you mean % s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word does not exist. Please double check it."
+        else:
+            return "We didn't get your entry."
     else:
         return "The word doesn't exist please double check it."
 
+word = input("Enter word: ")
 
-word = input("Enter word:")
+output = translate(word)
 
-print(translate(word))
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
+
